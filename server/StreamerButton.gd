6f313adcbf5df1d207459b2c0de01255921e3da4
Greaby <contact@greaby.com>
2018-05-05 09:09:04 +0200

    add streamer drag preview

--- server/StreamerButton.gd
+++ server/StreamerButton.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
 func get_drag_data(pos):
-	var server = load("res://server/assets/streamer.png")
-	set_drag_preview(server)
+	var streamer = load("res://server/StreamerPreview.tscn")
+	set_drag_preview(streamer.instance())
 	return "streamer"
